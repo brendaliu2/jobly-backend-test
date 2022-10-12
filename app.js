07ea2d3778db54dcdb96a0d367bdffa0f23f3463
Brenda Liu <brenda.liu24@gmail.com>
2022-10-12 13:25:45 -0500

    added google route

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const authRoutes = require("./routes/auth");
 const companiesRoutes = require("./routes/companies");
 const usersRoutes = require("./routes/users");
 const jobsRoutes = require("./routes/jobs");
+require('dotenv').config();
+
 
 const morgan = require("morgan");
 
@@ -26,6 +28,25 @@ app.use("/auth", authRoutes);
 app.use("/companies", companiesRoutes);
 app.use("/users", usersRoutes);
 app.use("/jobs", jobsRoutes);
+
+
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
+app.post("/api/v1/auth/google", async (req, res) => {
+    const { token }  = req.body
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID
+    });
+    const { name, email, picture } = ticket.getPayload();
+    const user = await db.user.upsert({
+        where: { email: email },
+        update: { name, picture },
+        create: { name, email, picture }
+    })
+    res.status(201)
+    res.json(user)
+})
 
 
 /** Handle 404 errors -- this matches everything */
